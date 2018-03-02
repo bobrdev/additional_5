f7835c765cc0909f64dba86b7c557e19fb937f4a
Drabinko Artur <artur.drabinko@gmail.com>
2018-03-02 19:40:47 +0300

    Added check len str

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
+  
+  if(str.length % 2 != 0) return false;
 
-  let strLen = str.length * bracketsConfig.length;
+  let strLen = Math.floor(str.length / 2);
   let bracketsLen = bracketsConfig.length;
 
   for (let pos = 0; pos < strLen; pos++) {
